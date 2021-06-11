68518cd970ff08ea8988c0ef346528137bc28eb6
HassanBahati <mukisabahati@gmail.com>
2021-06-12 02:00:37 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
       {/* header section   */}
       <header className="row">
         <div>
-          <a className="brand" href="index.html">
+          <a className="brand" href="/">
             Amazona
           </a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/sign-in">Sign In</a>
         </div>
       </header>
       {/* main section  */}
