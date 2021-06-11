402689360fc142273d2453883aee3823131c61af
HassanBahati <mukisabahati@gmail.com>
2021-06-11 21:12:26 +0300

    edited files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,186 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div class="row center">
+      {/* product item   */}
+      <div class="card">
+        <a href="product.html">
+          {/* image size: 680px by 830px    */}
+          <img class="medium" src="./images/product-1.jpeg" alt="product" />
         </a>
-      </header>
+        {/* card body   */}
+        <div class="card-body">
+          <a href="product.hrml">
+            <h2>Slim Shirts</h2>
+          </a>
+          {/* rating   */}
+          <div class="rating">
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+          </div>
+          {/*price section   */}
+          <div class="price">$120</div>
+        </div>
+      </div>
+      {/* product item  */}
+      <div class="card">
+        <a href="product.html">
+          {/* image size: 680px by 830px   */}
+          <img class="medium" src="./images/product-1.jpeg" alt="product" />
+        </a>
+        {/* card body   */}
+        <div class="card-body">
+          <a href="product.hrml">
+            <h2>Slim Shirts</h2>
+          </a>
+          {/* rating   */}
+          <div class="rating">
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+          </div>
+          {/* price section  */}
+          <div class="price">$120</div>
+        </div>
+      </div>
+      {/* product item   */}
+      <div class="card">
+        <a href="product.html">
+          {/* image size: 680px by 830px  */}
+          <img class="medium" src="./images/product-1.jpeg" alt="product" />
+        </a>
+        {/* card body   */}
+        <div class="card-body">
+          <a href="product.hrml">
+            <h2>Slim Shirts</h2>
+          </a>
+          {/* rating   */}
+          <div class="rating">
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+          </div>
+          {/* price section   */}
+          <div class="price">$120</div>
+        </div>
+      </div>
+      {/* product item   */}
+      <div class="card">
+        <a href="product.html">
+          {/* image size: 680px by 830px   */}
+          <img class="medium" src="./images/product-1.jpeg" alt="product" />
+        </a>
+        {/* card body   */}
+        <div class="card-body">
+          <a href="product.hrml">
+            <h2>Slim Shirts</h2>
+          </a>
+          {/* rating  */}
+          <div class="rating">
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+          </div>
+          {/* price section   */}
+          <div class="price">$120</div>
+        </div>
+      </div>
+      {/* product item   */}
+      <div class="card">
+        <a href="product.html">
+          {/* image size: 680px by 830px   */}
+          <img class="medium" src="./images/product-1.jpeg" alt="product" />
+        </a>
+        {/* card body   */}
+        <div class="card-body">
+          <a href="product.hrml">
+            <h2>Slim Shirts</h2>
+          </a>
+          {/* rating   */}
+          <div class="rating">
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+          </div>
+          {/* price section   */}
+          <div class="price">$120</div>
+        </div>
+      </div>
+      {/* product item   */}
+      <div class="card">
+        <a href="product.html">
+          {/* image size: 680px by 830px   */}
+          <img class="medium" src="./images/product-1.jpeg" alt="product" />
+        </a>
+        {/* card body   */}
+        <div class="card-body">
+          <a href="product.hrml">
+            <h2>Slim Shirts</h2>
+          </a>
+          {/* rating   */}
+          <div class="rating">
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star-half-o"></i>
+            </span>
+          </div>
+          {/* price section   */}
+          <div class="price">$120</div>
+        </div>
+      </div>
     </div>
   );
 }
