8ec0e5885504c68a84c7012fb6fc680d8021a1ff
HassanBahati <mukisabahati@gmail.com>
2021-06-12 20:16:10 +0300

    added redux homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,23 +1,25 @@
 import React, { useEffect } from "react";
-import {useSelector}from 'react-redux'
-import LoadingBox from '../components/LoadingBox'
-import MessageBox from '../components/MessageBox'
+import { useDispatch, useSelector } from "react-redux";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { listProducts } from "../actions/productActions"
+import Product from '../components/Product'
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
-  const productList = useSelector(state => state.productList)
-  const {loading, error, products} = productList;
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
     dispatch(listProducts());
-  }, []);
-
+  }, [dispatch]);
+ 
   return (
     <div>
       {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
-        <MessageBox variant='danger'>{error}</MessageBox>
+        <MessageBox variant="danger">{error}</MessageBox>
       ) : (
         <div className="row center">
           {products.map((product) => (
