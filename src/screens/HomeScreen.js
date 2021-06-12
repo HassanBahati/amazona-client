9173d70865126ff4f668779b61d9c5e9c3c86cf6
HassanBahati <mukisabahati@gmail.com>
2021-06-12 17:02:23 +0300

    added hooks

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import data from "../data";
 import Product from '../components/Product'
 
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(()=>{
+    const fetchData = async () =>{
+      const {data} = await axios.get('/api/products');
+    }
+  },[]);
+
   return (
     <div>
       <div className="row center">
