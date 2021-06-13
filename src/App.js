9c908c2a741d30a316eb5251351be7728eea2616
HassanBahati <mukisabahati@gmail.com>
2021-06-13 10:45:48 +0300

    added cart screen

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
 import React from "react";
-import {BrowserRouter, Link, Route} from 'react-router-dom'
+import { useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const {cartItems }  = cart;
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      {/* header section   */}
-      <header className="row">
-        <div>
-          <Link className="brand" to="/">
-            Amazona
-          </Link>
-        </div>
-        <div>
-          <Link to="/cart">Cart</Link>
-          <Link to="/sign-in">Sign In</Link>
-        </div>
-      </header>
-      {/* main section  */}
-      <main>
-        <Route path='/cart/:id?' component={CartScreen}></Route>
-        <Route path='/product/:id' component={ProductScreen}></Route>
-        <Route path='/' component={HomeScreen} exact></Route>
-        
-       
-      </main>
+      <div className="grid-container">
+        {/* header section   */}
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              Amazona
+            </Link>
+          </div>
+          <div> 
+            <Link to="/cart">Cart
+            {cartItems.length > 0 && (
+              <span className='badge'>{cartItems.length}</span>
+            )}
+            </Link>
+            <Link to="/sign-in">Sign In</Link>
+          </div>
+        </header>
+        {/* main section  */}
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
 
-      {/* footer section  */}
-      <footer className="row center">All right reserverd</footer>
-    </div>
+        {/* footer section  */}
+        <footer className="row center">All right reserverd</footer>
+      </div>
     </BrowserRouter>
   );
 }
